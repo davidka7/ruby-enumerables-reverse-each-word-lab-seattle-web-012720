a8a97c5d2e7980d1a618249bb0de838c8598b01a
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-18 07:44:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,8 @@
 
 def reverse_each_word(array)
-new=array.split("")
+new=array.split(" ")
  new_array=[]
  new_array = new.collect  {|index| index.reverse}
- new_array.join("")
+ new_array.join(" ")
 end
 
-def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
-end 
